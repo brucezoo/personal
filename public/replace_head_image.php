d487358d9f8404b321c60eecbbbb9207615e443e
Brucezoo <zhuf@iclockwork.com>
2017-04-21 13:52:24 +0800

    six

--- public/replace_head_image.php
+++ public/replace_head_image.php
@@ -1,13 +1,12 @@
 <?php
 session_start();
 require ("./test/smarty/main1.php");
-$db=new mysqli("127.0.0.1","root","root","testDatabase");
+date_default_timezone_set('prc');
+$db=new mysqli("localhost","root","root","testDatabase");
 if(mysqli_connect_errno()){
     echo "服务器繁忙，请稍后再试";
     exit;
 }
-mysqli_query($db,'set names utf8');
-
 $quer="select * from navs";
 $resu=$db->query($quer);
 
